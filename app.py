848a49f2c9e5f81b34018bbed690c692c2826b27
Mmukund <mvsslnmukund@gmail.com>
2025-07-09 14:36:47 +0530

    Change for render

--- app.py
+++ app.py
@@ -27,6 +27,8 @@ def predict():
     except Exception as e:
         return render_template("index.html", result=f"Error: {e}")
 
+import os
+
 if __name__ == '__main__':
-    print("🚀 Flask app is starting...")
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render assigns this
+    app.run(host='0.0.0.0', port=port)
